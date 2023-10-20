afe796e2eda7b0c8ceb69346a12d5ba8e1071937
Cláudio Silva <claudio.engdist@gmail.com>
2023-10-20 17:07:35 -0300

    setting correct sharding contract address

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const { Chess } = require('chess.js');
 const rollup_server = process.env.ROLLUP_HTTP_SERVER_URL;
 console.log("HTTP rollup_server url is " + rollup_server);
 
-const SHARDING_CONTRACT_ADDRESS = "0x90F79bf6EB2c4f870365E785982E1f101E93b906";
+const SHARDING_CONTRACT_ADDRESS = "0x4753D5746881907764A789Dd67FD62e3573844Ea";
 const INPUTBOX_CONTRACT_ADDRESS = "0x59b22D57D4f067708AB0c00552767405926dc768";
 const INPUTBOX_ADDINPUT_SELECTOR = "0x1789cd63";
 
@@ -25,12 +25,22 @@ async function handle_advance(data) {
     const mainDappAddress = payload;
     if (game.isGameOver()) {
       console.log("Creating voucher for " + winner);
-      await emitVoucher(INPUTBOX_CONTRACT_ADDRESS,mainDappAddress,winner);
+      await emitVoucher(
+        INPUTBOX_CONTRACT_ADDRESS,
+        INPUTBOX_ADDINPUT_SELECTOR,
+        mainDappAddress,
+        winner
+        );
     } else {
       if (winner==null) {
         console.log("Game is not over yet and no illegal move was provided. Why are you calling me?");
       } else {
-        await emitVoucher(INPUTBOX_CONTRACT_ADDRESS,mainDappAddress,winner);
+        await emitVoucher(
+          INPUTBOX_CONTRACT_ADDRESS,
+          INPUTBOX_ADDINPUT_SELECTOR,
+          mainDappAddress,
+          winner
+        );
       }
     }
 
@@ -66,11 +76,11 @@ function getAdversary(player) {
   return player.toLowerCase() == "w" ? "b" : "w";
 }
 
-async function emitVoucher(inputBoxAddress, mainDappAddress, winner) {
+async function emitVoucher(inputBoxAddress, addInputSelector, mainDappAddress, winner) {
   const types = ['address','bytes'];
   const values = [mainDappAddress,  ethers.toUtf8Bytes(winner)];
   const methodSignature = AbiCoder.defaultAbiCoder().encode(types, values);
-  const transferPayload = INPUTBOX_ADDINPUT_SELECTOR + methodSignature.slice(2);
+  const transferPayload = addInputSelector + methodSignature.slice(2);
   voucher = {"destination": inputBoxAddress, "payload": transferPayload};
 
   const voucher_req = await fetch(rollup_server + '/voucher', {
